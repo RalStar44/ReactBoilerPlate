9141d01bc1a6c5e50bd78689da9523de37ae0632
Ken Gee <kengee47@outlook.com>
2023-09-29 09:05:35 -0700

    refactor on data operator

--- src/data_access_layer/operator_factory.js
+++ src/data_access_layer/operator_factory.js
@@ -1,65 +1,42 @@
 /**
- *  @file operator_factory.js
- */ 
-import { useState, useEffect } from 'react';
+ * Factory function to create the appropriate data operator based on the data type.
+ * @param {string} dataType - The type of data operators to fetch.
+ * @returns {function} - The data operator function.
+ * @throws {Error} - If the provided data type is invalid.
+ */
 import dataOperatorFunctions from './data_operator_functions';
 
-/**
- * Custom hook to fetch and manage data operators based on the provided data type.
- * @param {string} dataType - The type of data operators to fetch.
- * @returns {object} - An object containing data operators, loading state, and error state.
- */
-const useDataOperators = (dataType) => {
-  // State variables to hold data operators, loading state, and error state
-  const [dataOperators, setDataOperators] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+const operatorFactory = (dataType) => {
+  const dataRetriever = dataOperatorFunctions[dataType];
 
-  /**
-   * Factory function to create the appropriate data operator based on the data type.
-   * @returns {function} - The data operator function.
-   * @throws {Error} - If the provided data type is invalid.
-   */
-  const operatorFactory = () => {
-    const dataRetriever = dataOperatorFunctions[dataType]; // Use dataOperatorFunctions here
-    
-    if (dataRetriever) {
-      return dataRetriever;
-    } else {
-      throw new Error(`Invalid data type: ${dataType}`);
-    }
-  };
-
-  /**
-   * Fetch data operators and update state accordingly.
-   * @returns {void}
-   */
-  const fetchDataOperators = async () => {
-    setIsLoading(true);
-
-    try {
-      const dataRetriever = operatorFactory();
-      const result = await dataRetriever();
-      setDataOperators(result);
-      setError(null);
-    } catch (error) {
-      setError(error);
-    }
-
-    setIsLoading(false);
-  };
-
-  // Fetch data operators when dataType changes
-  useEffect(() => {
-    fetchDataOperators();
-  }, [dataType]);
-
-  // Return an object containing data operators, loading state, and error state
-  return {
-    data: dataOperators,
-    isLoading,
-    error
-  };
+  if (dataRetriever) {
+    return dataRetriever;
+  } else {
+    throw new Error(`Invalid data type: ${dataType}`);
+  }
 };
 
-export default React.memo(useDataOperators);
+/**
+ * Fetch data operators and update state accordingly.
+ * @param {string} dataType - The type of data operators to fetch.
+ * @param {function} setDataOperators - Function to set the data operators state.
+ * @param {function} setIsLoading - Function to set the loading state.
+ * @param {function} setError - Function to set the error state.
+ * @returns {void}
+ */
+const fetchDataOperators = async (dataType, setDataOperators, setIsLoading, setError) => {
+  setIsLoading(true);
+
+  try {
+    const dataRetriever = operatorFactory(dataType);
+    const result = await dataRetriever();
+    setDataOperators(result);
+    setError(null);
+  } catch (error) {
+    setError(error);
+  }
+
+  setIsLoading(false);
+};
+
+export { operatorFactory, fetchDataOperators };
