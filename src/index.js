161770dc9e0e892ae3f29abbc92f6e648df6ebfa
Ken Gee <kengee47@outlook.com>
2023-09-29 07:54:32 -0700

    added featureDataContextProvider

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import App from './app';
 import featureTitle from './hoc/features/featureTitle';
 import featureLogging from './hoc/features/featureLogging';
 import featureTheme from './hoc/features/featureTheme';
+import featureContextProvider from './hoc/features/featureContextProvider';
 import applyFeatures from './hoc/loadFeatures';
 import configs from './config';
 
@@ -16,7 +17,7 @@ import configs from './config';
  * Enhance the App component with additional features using applyFeatures.
  * @type {React.Component}
  */
-const EnhancedApp = applyFeatures(App, [featureTitle, featureLogging, featureTheme]);
+const EnhancedApp = applyFeatures(App, [featureTitle, featureLogging, featureContextProvider, featureTheme]);
 
 // Get the root element from the HTML document.
 const rootElement = document.getElementById('root');
